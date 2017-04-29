e43d802b6c00559501f837c05a7cc82d840cd952
Elliot Young <elliotsyoung@gmail.com>
2017-04-29 12:55:23 -0700

    added basic components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from './components/common';
+
+const App = () => {
+  return (
+      <View style={{ flex: 1 }}>
+        <Header>Arctic Game</Header>
+      </View>
+  );
+}; // end of functional component App
+
+export default App;
